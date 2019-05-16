28b39a36aaeda0146044a1e76db96cfa5e9d71f2
matsumoto <matsumoto@cloverlab.jp>
2019-05-16 19:35:43 +0900

    DB dump

---
+++ sql/t_member_platforms.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `t_member_platforms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `t_member_id` int(11) NOT NULL COMMENT '会員ID',
+  `m_platform_id` int(11) NOT NULL COMMENT 'プラットフォーム',
+  `pfid` varchar(100) NOT NULL DEFAULT '' COMMENT 'プラットフォームのID',
+  `del_flg` tinyint(1) NOT NULL DEFAULT '0' COMMENT '削除フラグ',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `platform_id` (`m_platform_id`,`pfid`),
+  KEY `t_member_id` (`t_member_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ sql/t_members.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `t_members` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `user_id` varchar(20) NOT NULL COMMENT 'ユーザー指定ID',
+  `name` varchar(20) NOT NULL COMMENT '名前',
+  `email` varchar(100) NOT NULL COMMENT 'Eメール',
+  `password` varchar(200) NOT NULL DEFAULT '' COMMENT 'パスワード',
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
+  `ban` tinyint(1) NOT NULL DEFAULT '0',
+  `del_flg` tinyint(1) NOT NULL DEFAULT '0' COMMENT '削除フラグ',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
