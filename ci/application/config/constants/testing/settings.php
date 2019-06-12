4617b7e566ea3107a2658c12ae8433459e052a91
matsumoto <matsumoto@cloverlab.jp>
2019-06-12 16:19:44 +0900

    URLに入った余分なスラッシュを削除

--- ci/application/config/constants/testing/settings.php
+++ ci/application/config/constants/testing/settings.php
@@ -1,6 +1,6 @@
 <?php
 define("SITE_TITLE", "Team League");
-define("SITE_URL", "https://team-league.appspot.com/");
+define("SITE_URL", "https://team-league.appspot.com");
 
 define("APP_LOCAL_PATH", "/srv");
 define("DOCUMENT_ROOT_PATH", APP_LOCAL_PATH . "/public");
