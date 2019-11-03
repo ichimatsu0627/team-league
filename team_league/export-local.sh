f47c1b243a0db1be5e6e5d89cff170f0d1c4a100
ichimatsu0627 <crade.al.fato@gmail.com>
2019-11-03 21:51:09 +0900

    [Update]Carpenter shell fix

--- team_league/export-local.sh
+++ team_league/export-local.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-carpenter -vv -s teamleague -d "ichimatsu:password@tcp(127.0.0.1:12001)" design -s -p --dir ../database/json/
-carpenter -vv -s teamleague -d "ichimatsu:password@tcp(127.0.0.1:12001)" export -r "^m_.*" -d ../database/csv/
+./carpenter -vv -s teamleague -d "ichimatsu:password@tcp(127.0.0.1:12001)" design -s -p --dir ../database/json/
+./carpenter -vv -s teamleague -d "ichimatsu:password@tcp(127.0.0.1:12001)" export -r "^m_.*" -d ../database/csv/
