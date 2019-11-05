5e1f8bd9bbd9a537a25c784480005fa6070cd3d3
matsumoto <matsumoto@cloverlab.jp>
2019-11-05 10:38:57 +0900

    [Update]LigntOpenId更新

--- ci/application/views/other/profile.php
+++ ci/application/views/other/profile.php
@@ -8,17 +8,17 @@
                 <a target="_blank" href="https://twitter.com/ichimatsu0627"><img src="/assets/img/icon-twitter.png" height="20"></a>
             </p>
             <p>
-                <a href="https://paypal.me/ichimatsu0627?locale.x=ja_JP"><img src="/assets/img/donate_button.png" height="40"></a>
+                <a target="_blank" href="https://paypal.me/ichimatsu0627?locale.x=ja_JP"><img src="/assets/img/donate_button.png" height="40"></a>
             </p>
         </div>
         <p class="col-9 message">
             はじめまして<br>
             当サイトを作成した<span style="font-weight: bold;">Ichimatsu</span>です。<br>
             <br>
-            当サイトは、チームの管理やチームメンバー募集などが行えるサイトです。<br>
+            当サイトは、Rocket Leagueにおけるチームの管理やチームメンバー探し、チーム探しなどが行えるサイトです。<br>
             リリース時点では機能が少ないですが、今後スクリム募集を行ったり、定期リーグを開催しサイト内でチームレートを持てるように出来ていったらいいなと思っています。<br>
-            サービス運用、製作者のモチベーション維持の為、<span class="font-weight-normal">Donate</span>頂けたら嬉しいです。<br>
-            また、制作チームとしてご協力頂ける方も募集しておりますので気軽にご相談ください<br>
+            サービス運用、製作者のモチベーション維持の為、<a target="_blank" href="https://paypal.me/ichimatsu0627?locale.x=ja_JP"><span class="font-weight-normal">Donate</span></a>頂けたら嬉しいです。<br>
+            また、制作チームとしてご協力頂ける方も募集しておりますので気軽にお声掛けください<br>
             <br>
             仲良くマナーを守り快適にご利用ください。<br>
         </p>
